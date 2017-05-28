280d27104da8498d0bd8371e99e1e88beb304c8e
rafal <ra88fi@o2.pl>
2017-05-28 21:26:48 +0200

    fixed tab's

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 // Zadanie 8.6
 
 var a = prompt('Podaj pierwsza liczbe');
-		if (a == parseInt(a)) {
+if (a == parseInt(a)) {
     var b = prompt('Podaj druga liczbe');
-  }
-    if (b == parseInt(b)) {
-        summary = (a * a) + (2 * a * b) - (b * b);
-			     console.log(summary);
+}
+if (b == parseInt(b)) {
+    summary = (a * a) + (2 * a * b) - (b * b);
+    console.log(summary);
 		
-			 if (summary > 0) {
-				    alert('Liczba dodatnia');
-			 }
-			 else if (summary < 0) {
-				    alert('Liczba ujemna');	
-			 }
-			 else {
-				    alert('Wynik rowny zero');	
-			 }
-		}
-		else {
-    		alert("Liczbe prosilem a nie litere");
-		}
+if (summary > 0) {
+    alert('Liczba dodatnia');
+}
+else if (summary < 0) {
+    alert('Liczba ujemna');	
+}
+else {
+    alert('Wynik rowny zero');	
+}
+}
+else {
+    alert("Liczbe prosilem a nie litere");
+}
