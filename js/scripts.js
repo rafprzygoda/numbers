07004e7e121d44c8a50bd217163b7ce35c3ee523
rafal <ra88fi@o2.pl>
2017-05-29 23:22:37 +0200

    isNaN method added

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 // Excercise 8.6
-// New version of script, more simple. JSHint approved. 
+// isNaN method added 
 
 var a = prompt("podaj liczbę");
 var b = prompt("podaj liczbę");
 var number = (a * a) + (2 * a * b) - (b * b);
 console.log(number);
-if (a == parseInt(a) && b == parseInt(b) && number > 0) {
+if (isNaN('a') && isNaN('b') && number > 0) {
     alert(number + ' to jest liczba dodatnia');
-} else if (a == parseInt(a) && b == parseInt(b) && number < 0) {
+} else if (isNaN('a') && isNaN('b') && number < 0) {
     alert(number + ' to jest liczba ujemna');
-} else if (a == parseInt(a) && b == parseInt(b) && number === 0) {
+} else if (isNaN('a') && isNaN('b') && number === 0) {
     alert(number + ' wynik równy zero');
 } else {
     alert("nie podałes liczb");
